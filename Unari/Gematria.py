799b975b9f74608cc46980a2ebbd0787361d3d17
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-15 14:15:28 +0200

    value error

--- Unari/Gematria.py
+++ Unari/Gematria.py
@@ -2,8 +2,15 @@ class אות:
 
     def __init__(self, char):
         unipt = char.encode("utf-8")
-        if b'\xd7\x90' <= unipt <= b'\xd7\xAA' or \
-            b'\xd7\xB0' <= unipt <= b'\xd7\xB2':
+        if b'\xd7\x90' <= unipt <= b'\xd7\xAA':
             self.val = char
+        elif b'\xd7\xB0' <= unipt <= b'\xd7\xB2':
+            raise ValueError  # Yiddish digraph
+        else:
+            raise ValueError
 
-    def yid_seperate(self,char):
+    def __str__(self):
+        return self.val
+
+    def __int__(self):
+        return {}
