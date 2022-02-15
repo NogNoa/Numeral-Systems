7bee14aed722f70bb2cff1538201e2e9bf63f9c5
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-02-15 14:03:15 +0200

    treating yiddish digraphs

--- Unari/Gematria.py
+++ Unari/Gematria.py
@@ -2,13 +2,8 @@ class אות:
 
     def __init__(self, char):
         unipt = char.encode("utf-8")
-        if b'\xd7\x90' <= unipt <= b'\xd7\xAA':
+        if b'\xd7\x90' <= unipt <= b'\xd7\xAA' or \
+            b'\xd7\xB0' <= unipt <= b'\xd7\xB2':
             self.val = char
-        elif b'\xd7\xB0' <= unipt <= b'\xd7\xB2':
-            return
 
-class Yid(אות):
-    def __init__(self,char):
-        super().__init__(char)
-
-
+    def yid_seperate(self,char):
