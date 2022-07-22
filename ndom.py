400986a3309424e647e908f244a1022f6a47b88f
NogNoa <53447424+NogNoa@users.noreply.github.com>
2022-07-22 13:48:49 +0300

    ndom glue function

--- ndom.py
+++ ndom.py
@@ -1,24 +1,27 @@
 def ones(call: int):
-    return ['',  #0
-      "sas ",    #1
-      "thef ",   #2
-      "ithin ",  #3
-      "thonith ",#4
-      "meregh "  #5
+    return ['', #0
+      "sas",    #1
+      "thef",   #2
+      "ithin",  #3
+      "thonith",#4
+      "meregh"  #5
       ][call % 6]
 
 def low_sixes(call: int):
-    return ['',      #00
-      "mer ",        #10
-      "mer an thef ",#20
-      "tondor ",     #30
+    return ['',     #00
+      "mer",        #10
+      "mer an thef",#20
+      "tondor",     #30
       ][call %4]
+
+def glue(pre : str, post: str):
+    glue = bool(pre and post) * " abo "
+    return pre + glue + post
 
 def ndom(call: int):
     sases = ones(call)
     low_mers = low_sixes(call // 6)
-    glue = bool(sases and low_mers) * "abo "
-    back = low_mers + glue + sases
+    back = glue(low_mers, sases)
     return back
 
 for i in range(18+5):
